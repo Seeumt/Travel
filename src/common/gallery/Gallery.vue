6144f472bc94ae661cf19a3fc1556b92211014e8
REN XIANG <Seeumt@cumt.edu.cn>
2019-11-16 21:34:28 +0800

    Gallery 9-2

--- src/common/gallery/Gallery.vue
+++ src/common/gallery/Gallery.vue
@@ -1,13 +1,69 @@
 <template>
-    <div>gallery</div>
+    <div class="container" @click="handleGalleryClick">
+      <div class="wrapper">
+        <swiper :options="swiperOptions">
+          <swiper-slide v-for="(img,index) in imgs" :key="index">
+            <img  class="gallery-img" :src="img">
+          </swiper-slide>
+          <div class="swiper-pagination"  slot="pagination">
+          </div>
+        </swiper>
+      </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "CommonGallery"
+        name: "CommonGallery",
+        props:{
+          imgs:{
+              type:Array,
+              default() {
+                  return [
+
+                  ]
+              }
+          }
+        },
+        data() {
+            return {
+                swiperOptions:{
+                    pagination: '.swiper-pagination',
+                    paginationType: 'fraction',
+                    observeParents:true,
+                    observer:true
+                }
+            }
+        },
+        methods:{
+            handleGalleryClick() {
+                this.$emit('close')
+            }
+        }
     }
 </script>
 
 <style lang="stylus" scoped>
-
+  .container >>>.swiper-container
+    overflow inherit
+  .container
+    display flex
+    flex-direction column
+    justify-content center
+    z-index 99
+    position fixed
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background wheat
+    .wrapper
+      height 0
+      width 100%
+      padding-bottom 100%
+      .gallery-img
+        width 100%
+      .swiper-pagination
+        color salmon
+        bottom -1rem
 </style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,16 +1,38 @@
 <template>
-    <div class="banner">
+  <div>
+    <div class="banner" @click="handleBannerClick">
       <img class="banner-img" src="https://imgs.qunarzz.com/vc/1e/25/dd/f308ea7ce15e0deb561aa9d9e3.jpg_92.jpg">
         <div class="banner-info">
           <div class="banner-title">这里是内容</div>
           <div class="banner-number"><span class="iconfont banner-icon">&#xe63b;</span>39</div>
         </div>
     </div>
+  <common-gallery :imgs="imgs" v-show="showGallery" @close="handleGalleryClose"></common-gallery>
+  </div>
 </template>
 
 <script>
+  import CommonGallery from 'common/gallery/Gallery'
     export default {
-        name: "DetailBanner"
+        name: "DetailBanner",
+        components:{
+            CommonGallery
+        },
+        data() {
+            return{
+                showGallery:false,
+                imgs:['https://imgs.qunarzz.com/vc/aa/9e/82/14bf523635542806a671763e98.jpg_92.jpg',
+                    'https://imgs.qunarzz.com/vc/6d/c8/f8/64968edbb09cd02cc08b2c89fa.jpg_92.jpg']
+            }
+        },
+        methods:{
+            handleBannerClick() {
+                this.showGallery=true
+            },
+            handleGalleryClose() {
+                this.showGallery=false;
+            }
+        }
     }
 </script>
 
